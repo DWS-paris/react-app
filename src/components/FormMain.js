5ea37a9c7716a923a4b65b840e9ffa22bd08d490
Julien Noyer <j.noyer@digitalworkshop.fr>
2023-01-24 09:12:02 +0100

    Clear form

--- src/components/FormMain.js
+++ src/components/FormMain.js
@@ -18,36 +18,7 @@
         constructor( props ){
             // ES6 => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
             super( props );
-
-            this.cmpFormValue = this.props.formvalue;
-            this.state = {  }
-            for( let item of this.cmpFormValue ){
-                this.state[ item.name ] = item.value;
-            }
-
-            // Bind 'this within the needed methods
-            this.onSubmit = this.onSubmit.bind( this );
-            this.onChangeState = this.onChangeState.bind( this );
         }
-
-        /* 
-            [CMP] Methods
-        */
-            onSubmit( event ){
-                // Stop event
-                event.preventDefault();
-                
-                // Emit event to send value to parent component
-                this.props.onSubmit( this.state )
-            }
-
-            onChangeState( event ){
-                // Use React set state method
-                this.setState( { 
-                    [ event.target.getAttribute('name') ]: event.target.value
-                })
-            }
-        //
 
         /* 
             [CMP] Render
@@ -57,34 +28,9 @@
             return(
                 <form onSubmit={ this.onSubmit }>
 
-                    { 
-                        this.cmpFormValue.map( (input, idx) => {
-                            return(
-                                <fieldset
-                                    key={ 'form-input-' + idx }
-                                >
-                                    <label 
-                                        htmlFor={ input.name }
-                                    >
-                                        { input.label }
-                                    </label>
+                    <p>add form</p>
 
-                                    <input 
-                                        name={ input.name }
-                                        type={ input.type }
-                                        placeholder={ input.placeholder }
-                                        min={ input.min ? input.min : null }
-                                        required={ input.required ? input.required : false }
-                                        
-                                        value={ this.state[input.name] }
-                                        onChange={ this.onChangeState }
-                                    />
-                                </fieldset>
-                            )
-                        }) 
-                    }
-
-                    <button type='submit'>Connexion</button>
+                    <button type='submit'>OK</button>
                 </form>
             )
         }
